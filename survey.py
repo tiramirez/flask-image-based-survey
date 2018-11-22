bc306089437d410969f791786c5d8e53f2b23c18
tiramirez <tiramirez@uc.cl>
2018-11-22 19:31:58 -0300

    comenzar a crear init

--- survey.py
+++ survey.py
@@ -1,10 +1,48 @@
+import os
 from flask import Flask
 
-app = Flask(__name__, static_url_path='/static')
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'survey.sqlite'),
+    # )
 
-@app.route('/')
-def index():
-	return 'Hola Javi!!'
+    @app.route('/')
+	def index():
+		return 'Hola Javi!!'
 
-if __name__ == "__main__":
-	app.run()
+
+ #    if test_config is None:
+ #        # load the instance config, if it exists, when not testing
+ #        app.config.from_pyfile('config.py', silent=True)
+ #    else:
+ #        # load the test config if passed in
+ #        app.config.from_mapping(test_config)
+
+ #    # ensure the instance folder exists
+ #    try:
+ #        os.makedirs(app.instance_path)
+ #    except OSError:
+ #        pass
+
+ #    # intialize database
+ #    from . import db
+ #    db.init_app(app)
+
+	# # Import and register the blueprint
+ #    from . import auth
+ #    app.register_blueprint(auth.bp)
+ #    app.add_url_rule('/', endpoint='register')
+
+    return app
+
+# app = Flask(__name__, instance_relative_config=True)
+
+# @app.route('/')
+# def index():
+# 	return 'Hola Javi!!'
+
+# if __name__ == "__main__":
+# 	app.run()
