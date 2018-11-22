fff5b10d0c555998c4eafbd2dfec7cf00d1215d6
tiramirez <tiramirez@uc.cl>
2018-11-22 19:44:02 -0300

    caperta survey

--- survey/__init__.py
+++ survey/__init__.py
@@ -1,17 +1,17 @@
-import os
+# import os
 from flask import Flask
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'survey.sqlite'),
-    # )
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     # app.config.from_mapping(
+#     #     SECRET_KEY='dev',
+#     #     DATABASE=os.path.join(app.instance_path, 'survey.sqlite'),
+#     # )
 
-    @app.route('/')
-	def index(): 
-		return 'Hola Javi!!'
+#     @app.route('/')
+# 	def index(): 
+# 		return 'Hola Javi!!'
 
 
  #    if test_config is None:
@@ -36,13 +36,13 @@ def create_app(test_config=None):
  #    app.register_blueprint(auth.bp)
  #    app.add_url_rule('/', endpoint='register')
 
-    return app
+    # return app
 
-# app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
-# @app.route('/')
-# def index():
-# 	return 'Hola Javi!!'
+@app.route('/')
+def index():
+	return 'Hola Javi!!'
 
 if __name__ == "__main__":
 	app.run()
