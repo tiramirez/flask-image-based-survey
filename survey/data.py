add7d46a72ad17e355f10f87784a728c8b99b7ae
tiramirez <tiramirez@uc.cl>
2018-11-22 23:27:39 -0300

    montar framework

---
+++ survey/data.py
@@ -0,0 +1,5 @@
+ACTORS = [
+{"id":'0001',"name":"Foto1","photo":"/images/IMG_01.jpg"},
+{"id":'0002',"name":"Foto1","photo":"./images/IMG_01.jpg"},
+{"id":'0003',"name":"Foto1","photo":"/static/images/IMG_01.jpg"},
+{"id":'0004',"name":"Foto1","photo":"/static/images/IMG_01.jpg"}]
