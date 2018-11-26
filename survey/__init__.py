84ac9f52977606411ba61fd3bc070018f5302bf5
tiramirez <tiramirez@uc.cl>
2018-11-26 15:00:55 -0300

    implementar SQLalchemy

--- survey/__init__.py
+++ survey/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgres://rpvhhvddjfxgnq:508f6ee01161c48c8f320ab07949e225febd2fc35a60a61653c3b5e67d39dc1e@ec2-54-225-110-156.compute-1.amazonaws.com:5432/de9brngo4iedlo']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # Initializing the manager
