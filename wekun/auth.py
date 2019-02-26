8bcf3b41158374b231b6eb66fda932449564ba24
tiramirez <tiramirez@uc.cl>
2019-02-26 16:46:16 -0300

    lenguajes cookies and changes

--- wekun/auth.py
+++ wekun/auth.py
@@ -13,8 +13,38 @@ bp = Blueprint('auth', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('survey.html')
+
+    ## Primero se revisa que exista la Cookie 'userid' y que corresponda a un id en la Tabla Users
+    username = request.cookies.get('userid')
+    counter = 0
+    languaje = request.cookies.get('languaje')
+    # counter  = db.session.query(func.count()).filter(Users.id == username).all()[0][0]
+
+    ## En caso de cumplirse ambos criterios, se redirige a la pagina home
+    if username and counter > 0:
+        # print("Hay cookies")
+        return redirect(url_for('auth.survey'))
+
+    # # En caso contrario se redirige a la pagina de registro
+    # print("No hay cookies")
+    return redirect(url_for('auth.welcome'))
 
 @bp.route('/about_us', methods=('GET', 'POST'))
 def about_us():
     return "Hello world"
+
+@bp.route('/welcome', methods=('GET', 'POST'))
+def welcome():
+    return render_template('welcome.html')
+
+@bp.route('/es', methods=('GET','POST'))
+def spanish():
+    resp = make_response(redirect('/'))
+    resp.set_cookie('languaje', 'es', max_age=60*60*24*365)
+    return resp
+
+@bp.route('/en', methods=('GET','POST'))
+def english():
+    resp = make_response(redirect('/'))
+    resp.set_cookie('languaje', 'en', max_age=60*60*24*365)
+    return resp
