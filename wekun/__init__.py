ddb3993e36c06f4a822cf13b347b0efe532259a1
tiramirez <tiramirez@uc.cl>
2019-03-25 12:21:21 -0300

    responsive menu

--- wekun/__init__.py
+++ wekun/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
+ 
 
 # Initializing the manager
 db = SQLAlchemy(app)
