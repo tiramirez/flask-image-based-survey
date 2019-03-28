5f6ff11db8178a3b523f001a0d9409828600b050
tiramirez <tiramirez@uc.cl>
2019-03-28 14:46:22 -0300

    wgsi

--- wekun/__init__.py
+++ wekun/__init__.py
@@ -2,19 +2,19 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
-app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'	
+application = Flask(__name__, instance_relative_config=True, static_url_path='/static')
+application.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'	
 
 ## Change this when runnign on local/external serve
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
  
 
 # Initializing the manager
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 # Import and register the blueprint
 from wekun import auth
-app.register_blueprint(auth.bp)
-app.add_url_rule('/', endpoint='index')
+application.register_blueprint(auth.bp)
+application.add_url_rule('/', endpoint='index')
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from wekun import app as application
+from wekun import application
 
 if __name__ == "__main__":
 	application.run()
