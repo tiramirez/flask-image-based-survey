8eae6ba643fb165c5c2063208d29d4710a1f7ae8
tiramirez <tiramirez@uc.cl>
2021-11-19 12:47:30 -0300

    Change directory /wekun to /survey

--- survey/__init__.py
+++ survey/__init__.py
@@ -1,19 +1,20 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
-app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'	
+
+## Change this when runnign on local/external serve
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+ 
 
 # Initializing the manager
 db = SQLAlchemy(app)
 
 # Import and register the blueprint
-from survey import auth
+from wekun import auth
 app.register_blueprint(auth.bp)
-app.add_url_rule('/', endpoint='register')
-
-
+app.add_url_rule('/', endpoint='index')
